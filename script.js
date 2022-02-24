3f9a7f731e11f636957b43ca5cf30f9104955b75
sarahjswasey <97562027+sarahjswasey@users.noreply.github.com>
2022-02-23 17:01:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// STEP 1
+    // let someMonth; 
+    // function theMonth(); 
+    // let currentMonth; 
+    // let summerMonth; 
+    // let MyLibraryFunction;
+
+// STEP 2
+    // numeric literal expression: 98.6
+    // string literal expression: 'my name is sarah'
+    // Boolean literal expression: true
+    // null literal expression: null
+
+// STEP 3
+    // 1. var expressionA = 4 * (2 / 1) + 8;
+    // 2. var expressionB = expressionA * radius;
+
+// STEP 4 
+    // 1. var firstName;
+    // 2. var lastName;
+    // 3. var address;
+    // 4. var city;
+    // 5. var state;
+    // 6. var zipCode;
+    // 7. var yourAge;
+    // 8. var referralSource;
+    // 9. var mayWeContactYou;
+
+// STEP 5 
+    // var firstName;
+    // var lastName;
+    // var yourAge;
+    // firstName = 'Sarah';
+    // lastName = 'Swasey';
+    // yourAge = 100;
+
+    // var firstName = 'Sarah';
+    // var lastName = 'Swasey';
+    // var yourAge = 100;
+
+    // var firstName = 'Sarah', lastName = 'Swasey', yourAge = 100;
+
+// STEP 6 
+    // var myName = 'Sarah Swasey', yearsAlive = 32;
+    // window.console.log(`My name is ${myName} and I am ${yearsAlive} years old.`); 
+
+// STEP 7 
+    // Part 1
+        // var myName = 'Sarah';
+        // let nombre = true;
+        // console.log(`My friend ${myName} is nice; this is ${nombre}`); 
+
+    // Part 2
+        // var faveNum = 5;
+        // var num = true;
+        // console.log(`${faveNum} is my favorite number: this is ${num}`); 
+
+// STEP 8
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'; 
+    // window.console.log(someString);
+
+// STEP 9
+    // let x = null;
+    // console.log(x);
+
+    // let y;
+    // console.log(y);
+
+// STEP 10
+    // String
+        // let x = 'Hello friends!';
+        // console.log(typeof(x));
+
+    // Number
+        // let x = 5;
+        // let y = 2;
+        // console.log(typeof(x + y));
+
+    // Boolean
+        // let y = true;
+        // console.log(typeof(y));
+
+    // Object
+        // console.log(typeof{x:1, y:2});
+
+    // Undefined
+        // let x;
+        // console.log(x);
+
+// STEP 11
+    // let x = 'Hello Sarah Swasey';
+    // let y = ', welcome to the JavaScript class!';
+    // alert(x + y);
+
+// STEP 12
+    // var name = 'Sarah Swasey';
+    // let x = 'Hello';
+    // let y = ', welcome to the JavaScript class!';
+    // alert(x + ' ' + `${name}` + y);
+
+// STEP 13
+    // var name = 'Sarah Swasey', course = 'JavaScript';
+    // let x = 'Hello';
+    // let y = ', welcome to the';
+    // let z = 'class!';
+    // alert(x + ' ' + `${name}` + y + ' ' + `${course}` + ' ' + z);
+
+// STEP 14 
+    // var name = 'Sarah Swasey', course = 'JavaScript';
+    // alert('Hello ' + name + '.' + '\n' + 'Welcome to the ' + course + ' class!');
+
+// STEP 15 
+    // let result = window.prompt('What is your name?');
+    // window.console.log(result)
+
+// STEP 16 
+    // let result = window.prompt('What class are you taking?');
+    // window.console.log(result);
+
+// STEP 17
+    // let x = 10;
+    // let y = 20;
+    // console.log(x + y);
+
+// STEP 18
+    // let x = 20;
+    // console.log(x + 20);
+
+// STEP 19
+    // let x = 20;
+    // let y = 5;
+    // console.log(x * y);
+
+// STEP 20
+    // let x = 20 % 3;
+    // x /= 
+    // console.log(x);
+
+// STEP 21
+    // let fname = 'Sarah';
+    // let firstName = 'Sarah';
+    // console.log(fname === firstName);
+
+// STEP 22
+    // let x = 5;
+    // let y = 10;
+    // console.log(x == y);
